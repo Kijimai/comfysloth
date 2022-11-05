13c6a90b6187075cea86b5a60c0cb86cb77b895b
John Deen Bucog <JDBucog@yahoo.com>
2022-11-04 19:01:17 -0700

    include pages into one indexJS, add Routes in main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Navbar, Sidebar, Footer } from "./components"
+import styled from "styled-components"
+import {
+  Home,
+  About,
+  Cart,
+  Products,
+  SingleProduct,
+  Checkout,
+  Error,
+} from "./pages/index"
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
+
+const Button = styled.button``
+
+const Container = styled.div`
+  background-color: red;
+  color: white;
+`
 
 export default App
