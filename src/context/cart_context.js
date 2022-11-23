696bf3933307e7d7d50b036b11a0bdc3d02481ff
John Deen Bucog <JDBucog@yahoo.com>
2022-11-22 18:57:24 -0800

    setup CartPage component -- return a placeholder content for when cart is empty, initialize CartContent component inside CartPage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -20,12 +20,22 @@ const CartContext = React.createContext()
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  const addToCart = ( id, color, amount, product ) => {
+  const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
   }
 
+  const removeCartItem = (id) => {}
+
+  const toggleAmount = (id, value) => {}
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeCartItem, toggleAmount, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   )
